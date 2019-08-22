442e9f8d1078170aae7544bf35cda17919113e02
RebelUkraine <support4uf1@gmail.com>
2019-08-22 22:45:49 +0300

    Create unit_test.py

---
+++ unit_test.py
@@ -0,0 +1,9 @@
+import unittest
+
+from final import db, app, Data
+
+
+class test(unittest.TestCase):
+    def test_DB(self):
+        symbol = "MSFT,AMZN,AAPL,GOOGL,FB"
+        self.assertEqual(symbol, "MSFT,AMZN,AAPL,GOOGL,FB")
