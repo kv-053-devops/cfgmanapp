31180b1c95a2eb0c2160388ec7157316bc9f5734
OksanaMurdza <31917260+OksanaMurdza@users.noreply.github.com>
2019-10-03 20:25:42 +0300

    Update final.py

--- final.py
+++ final.py
@@ -96,14 +96,14 @@ def request_take():
         if query_type == "realtime":
             results = {"remote_api_url": "https://api.worldtradingdata.com",
                        "query_template": "/api/v1/stock?",
-                       "remote_api_token": key[token]}
+                       "remote_api_token": "ozJJnnNAuL6d7f80upQobuq04lRjB0Mc4DqseClvEgONmPz6bxTSN3GkG9qP"}
 
             return jsonify(results)
 
         elif query_type == "intraday":
             results = {"remote_api_url": "https://intraday.worldtradingdata.com",
                        "query_template": "/api/v1/intraday?",
-                       "remote_api_token": key[token]}
+                       "remote_api_token": "ozJJnnNAuL6d7f80upQobuq04lRjB0Mc4DqseClvEgONmPz6bxTSN3GkG9qP"}
             return jsonify(results)
     else:
         return "UNKNOWN QUERY TYPE"
